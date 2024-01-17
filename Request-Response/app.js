f79fbee5619cc62ea03de54fc6ef5f894f149332
Rushikesh Nagapurkar <148316559+Rushi1275@users.noreply.github.com>
2024-01-17 16:01:53 +0530

    Add files via upload

--- Request-Response/app.js
+++ Request-Response/app.js
@@ -1,1 +1,25 @@
+const http = require('http');
 
+const server = http.createServer((req, res) => {
+   
+    const URL = req.url;
+
+    res.setHeader('Content-Type', 'text/plain');
+
+ 
+    if (URL === '/home') {
+        res.end('Welcome home');
+    } else if (URL === '/about') {
+        res.end('Welcome to About Us page');
+    } else if (URL === '/node') {
+        res.end('Welcome to my Node Js project');
+    } else {
+        
+        res.statusCode = 404;
+        res.end('Page not found');
+    }
+});
+
+server.listen(4000, () => {
+    console.log('Server is running on port 4000');
+});
