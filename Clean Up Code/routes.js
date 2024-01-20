112fc052c2bd0bfa2b7fe6a059aaa783d9ac29ac
Rushikesh Nagapurkar <148316559+Rushi1275@users.noreply.github.com>
2024-01-20 17:37:55 +0530

    Add files via upload

---
+++ Clean Up Code/routes.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+
+const requestHandler = (req,res)=>{
+  
+const url = req.url;
+const method = req.method;
+
+if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body>');
+    res.write('<form action="/message" method="POST">');
+    res.write('<input type="text" name="message">');
+    res.write('<button type="submit">Send</button>');
+
+    // Read the content of message.txt and display it if available
+    fs.readFile('message.txt', 'utf-8', (err, data) => {
+        if (!err) {
+            res.write(`<p>${data}</p>`);
+        }
+        res.write('</form>');
+        res.write('</body>');
+        res.write('</html>');
+        res.end(); // Move res.end() here
+    });
+} else if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+        console.log(chunk);
+        body.push(chunk);
+    });
+    req.on('end', () => {
+        const parseBody = Buffer.concat(body).toString();
+        const message = parseBody.split('=')[1];
+        fs.writeFile('message.txt', message, (err) => {
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            res.end();
+        });
+    });
+} else {
+    // Move the common response headers and content outside the if conditions
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from node js server</h1></body>');
+    res.write('</html>');
+    res.end();
+}
+
+}
+
+//module.exports = requestHandler;
+
+//   module.exports ={
+//     handler:requestHandler,
+//     someText: "Some hard coded text"
+//   }
+
+exports.handler = requestHandler;
+exports.someText= "Some hard coded text";
