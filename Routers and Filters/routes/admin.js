d67343f5beba5385e101fef568ddf5554286743a
Rushikesh Nagapurkar <148316559+Rushi1275@users.noreply.github.com>
2024-01-29 17:04:34 +0530

    Add files via upload

--- Routers and Filters/routes/admin.js
+++ Routers and Filters/routes/admin.js
@@ -1,1 +1,17 @@
+const express =require ('express');
 
+const router = express.Router()
+
+router.get('/add-product', (req, res, next) => {
+    res.send('<body><form action="/admin/add-product" method="POST">' +
+        '<input type="text" name="productName" placeholder="Product Name">' +
+        '<button type="submit">Add Product</button></form></body>');
+});
+
+router.post('/add-product', (req, res, next) => {
+    console.log('Product Name:', req.body.productName);
+    res.redirect('/');
+  
+});
+
+module.exports=router
---
+++ Routers and Filters/routes/shop.js
@@ -0,0 +1,11 @@
+const express = require ('express');
+
+const router = express.Router();
+
+router.get('/product', (req, res, next) => {
+    res.send('<h1>This is Shop Page</h1>');
+});
+
+
+
+module.exports= router
